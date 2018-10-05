5032ca552ffacbbe68905fdd3b6e8333c5570018
feldlime <feldlime@yandex.ru>
2018-10-05 19:30:07 +0300

    Некоторые улучшения

--- figures.py
+++ figures.py
@@ -15,7 +15,7 @@ class Figure:
     Parameters
     ----------
     base: tuple, optional, default (0, 0)
-        Pair of coordinates for base point of figure.
+        Pair of coordinates (int or float) for base point of figure.
     angle: int or float, optional, default 0
         Angle (in radians) of rotation of figure with response to Ox.
     """
@@ -27,8 +27,7 @@ class Figure:
           or not (isinstance(base[1], int) or isinstance(base[1], float)):
             raise IncorrectParamType('Incorrect type of base')
 
-        if not (isinstance(angle, int) or isinstance(angle, float)):
-            raise IncorrectParamType('Incorrect type of angle')
+        self._validate_num(angle, 'angle')
 
         self._base = (float(base[0]), float(base[1]))
         self._i_angle = self._simplify_angle(float(angle))
@@ -55,15 +54,13 @@ class Figure:
         ------
          self
         """
-        if not (isinstance(dx, int) or isinstance(dx, float)):
-            raise IncorrectParamType('Incorrect type of dx')
-        if not (isinstance(dy, int) or isinstance(dy, float)):
-            raise IncorrectParamType('Incorrect type of dy')
-        self._base += (dx, dy)
+        self._validate_num(dx, 'dx')
+        self._validate_num(dy, 'dy')
+        self._base = self._base[0] + dx, self._base[1] + dy
         return self
 
     def rotate(self, angle):
-        """Move figure.
+        """Rotate figure.
 
         Parameters
         ----------
@@ -74,26 +71,35 @@ class Figure:
         ------
          self
         """
-        if not (isinstance(angle, int) or isinstance(angle, float)):
-            raise IncorrectParamType('Incorrect type of angle')
+        self._validate_num(angle, 'angle')
         self._angle += angle
         return self
 
     def get_base_representation(self):
         """Return special representation of figure that not contains angles."""
-        raise NotImplementedError
+        return NotImplemented
 
     @staticmethod
     def _simplify_angle(angle):
         return angle % (2 * np.pi)
 
     @staticmethod
-    def _validate_positive(value, parameter_name):
+    def _validate_num(value, parameter_name):
+        if not (isinstance(value, int) or isinstance(value, float)):
+            raise IncorrectParamError(f'Incorrect type of {parameter_name}')
+
+    @staticmethod
+    def _validate_positive_num(value, parameter_name):
         if not (isinstance(value, int) or isinstance(value, float)):
             raise IncorrectParamError(f'Incorrect type of {parameter_name}')
         if value <= 0:
             raise IncorrectParamError(f'{parameter_name.capitalize()} '
                                       f'must be positive')
+
+    def __repr__(self):
+        desc = f'{self.__class__.__name__} with base representation: ' \
+               f'{self.get_base_representation()}'
+        return desc
 
 
 class Point(Figure):
@@ -101,14 +107,15 @@ class Point(Figure):
 
     Parameters
     ----------
-    base: tuple, optional, default (0, 0)
-        Pair of coordinates for base point of figure.
+    coordinates: tuple, optional, default (0, 0)
+        Pair of point coordinates (int or float).
     """
-    def __init__(self, base=(0, 0)):
-        super().__init__(base=base)
+    def __init__(self, coordinates=(0, 0)):
+        super().__init__(base=coordinates)
 
     def rotate(self, angle):
-        raise NotImplementedError
+        """Not implemented here."""
+        return NotImplemented
 
     def get_base_representation(self):
         """Return special representation of figure that not contains angles.
@@ -120,19 +127,23 @@ class Point(Figure):
         return self._base
 
     @classmethod
-    def from_coordinates(cls, coordinates=(0, 0)):
+    def from_coordinates(cls, x, y):
         """Creates point from its coordinates.
 
         Parameters
         ----------
-        coordinates: tuple, optional, default 0
-            Coordinates (x, y) of point.
+        x: int or float
+            Coordinate x of point.
+        y: int or float
+            Coordinate y of point.
 
         Return
         ------
-        point: Point
+        point: Point instance
         """
-        return cls(base=coordinates)
+        cls._validate_num(x, 'x')
+        cls._validate_num(y, 'y')
+        return cls(coordinates=(x, y))
 
 
 class Segment(Figure):
@@ -140,16 +151,16 @@ class Segment(Figure):
 
     Parameters
     ----------
-    base: tuple, optional, default (0, 0)
+    start: tuple, optional, default (0, 0)
         Pair of coordinates for start point of segment.
     angle: int or float, optional, default 0
         Angle (in radians) between segment and Ox.
     length: int or float, optional, default 1
         Length of segment.
     """
-    def __init__(self, base=(0, 0), angle=0, length=1):
-        super().__init__(base, angle)
-        self._validate_positive(length, 'length')
+    def __init__(self, start=(0, 0), angle=0, length=1):
+        super().__init__(start, angle)
+        self._validate_positive_num(length, 'length')
         self._i_length = float(length)
 
     @property
@@ -158,7 +169,7 @@ class Segment(Figure):
 
     @_length.setter
     def _length(self, value):
-        self._validate_positive(value, 'length')
+        self._validate_positive_num(value, 'length')
         self._i_length = float(value)
 
     def get_base_representation(self):
@@ -173,6 +184,28 @@ class Segment(Figure):
         y2 = y1 + self._length * np.sin(self._angle)
         return x1, y1, x2, y2
 
+    @classmethod
+    def from_points(cls, x1, y1, x2, y2):
+        """Create segment from its end points.
+
+        x1: int or float
+            Coordinate x of the start of segment.
+        y1: int or float
+            Coordinate y of the end of segment.
+        x2: int or float
+            Coordinate x of the start of segment.
+        y2: int or float
+            Coordinate y of the end of segment.
+        """
+        cls._validate_num(x1, 'x1')
+        cls._validate_num(y1, 'y1')
+        cls._validate_num(x2, 'x2')
+        cls._validate_num(y2, 'y2')
+
+        dx, dy = x2 - x1, y2 - y1
+        length = np.sqrt(dx ** 2 + dy ** 2)
+        angle = np.arctan()  # TODO: Think about angles > pi / 2
+
 
 class Circle(Figure):
     """Class of circle.
@@ -186,7 +219,7 @@ class Circle(Figure):
     """
     def __init__(self, center=(0, 0), radius=1):
         super().__init__(base=center)
-        self._validate_positive(radius, 'radius')
+        self._validate_positive_num(radius, 'radius')
         self._i_radius = float(radius)
 
     @property
@@ -195,11 +228,11 @@ class Circle(Figure):
 
     @_radius.setter
     def _radius(self, value):
-        self._validate_positive(value, 'radius')
+        self._validate_positive_num(value, 'radius')
         self._i_radius = float(value)
 
     def rotate(self, angle):
-        raise NotImplementedError
+        return NotImplemented
 
     def get_base_representation(self):
         """Return special representation of figure that not contains angles.
@@ -228,7 +261,7 @@ class Arc(Figure):
     def __init__(self, center=(0, 0), radius=1, center_angle=np.pi, angle=0):
         super().__init__(base=center, angle=angle)
 
-        self._validate_positive(radius, 'radius')
+        self._validate_positive_num(radius, 'radius')
         self._i_radius = float(radius)
 
         self._validate_center_angle(center_angle)
@@ -240,7 +273,7 @@ class Arc(Figure):
 
     @_radius.setter
     def _radius(self, value):
-        self._validate_positive(value, 'radius')
+        self._validate_positive_num(value, 'radius')
         self._i_radius = float(value)
 
     @property
@@ -254,16 +287,27 @@ class Arc(Figure):
 
     def get_base_representation(self):
         """Return special representation of figure that not contains angles.
+            Represent arc as coordinates of its ends and radius.
 
         Return
         ------
-        x_center, y_center, radius: float
+        x1, y1, x2, y2, radius: float
         """
-        return self._base[0], self._base[1], self._radius
+        xc, yc = self._base  # center
+        radius = self._radius
+
+        angle = self._angle
+        x1 = xc + radius * np.cos(angle)
+        y1 = yc + radius * np.sin(angle)
+
+        angle = self._angle + self._center_angle
+        x2 = xc + radius * np.cos(angle)
+        y2 = yc + radius * np.sin(angle)
+        return x1, y1, x2, y2, radius
 
     @classmethod
     def _validate_center_angle(cls, angle):
-        cls._validate_positive(angle, 'center angle')
+        cls._validate_positive_num(angle, 'center angle')
         if angle >= 2 * np.pi:
             raise IncorrectParamError('Parameter  '
                                       'must be positive')
