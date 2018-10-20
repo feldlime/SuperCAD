aea621ae2e199301b2bc9d99c9e4cafc165dfd1a
Svit <Svyatoslav.EM@gmail.com>
2018-10-20 21:17:51 +0300

    Рисование линий и точек
    На левую кнопку рисуются линии, на правую точки

--- frontend/main.py
+++ frontend/main.py
@@ -20,15 +20,20 @@ class Helper(object):
         self.point_end = [0, 0]
         self.point_start = [0, 0]
 
-    def paint(self, painter, event, center_width, center_height, s):
+    def paint(self, painter, event, center_width, center_height, segments_array, points_array):
         painter.fillRect(event.rect(), QBrush(QColor(255, 255, 255)))
+        # Настраиваем кисть
+        painter.setPen(QPen(Qt.black, 2, Qt.SolidLine))
         painter.save()
         painter.translate(center_width, center_height)
-        for line in s:
+        # Рисуем все линии
+        for line in segments_array:
             to_draw = line.get_base_representation()
             painter.drawLine(*to_draw)
-
-        # print('paint', len(s))
+        # Рисуем все точки
+        for point in points_array:
+            to_draw = point.get_base_representation()
+            painter.drawEllipse(*to_draw, 2, 2)
         painter.restore()
 
 
@@ -40,18 +45,25 @@ class GLWidget(QOpenGLWidget):
         self.elapsed = 0
         self.setAutoFillBackground(True)
 
+        # Располагаем виджет в области work_plane и присваеваем ему те же паркаметры как в design
         self.setGeometry(QtCore.QRect(0, 0, parent.work_plane.width(), parent.work_plane.height()))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
 
+        # Вычисляем центр поля
         self.center_height = parent.work_plane.height() // 2
         self.center_width = parent.work_plane.width() // 2
 
         self.setSizePolicy(sizePolicy)
-        # s1 = Segment()
-        self.s = []
+
+        # Для рисования линнии, что бы знать, когда рисование окончено, можно так же найти как отслеживать отпускание
+        # левой кнопки мыши, но я не нашел
+        self.drag_to_print = False
+        # Массив хранения линий и точек
+        self.segments_array = []
+        self.points_array = []
 
     def animate(self):
         self.update()
@@ -60,31 +72,41 @@ class GLWidget(QOpenGLWidget):
         painter = QPainter()
         painter.begin(self)
         painter.setRenderHint(QPainter.Antialiasing)
-        self.helper.paint(painter, event, self.center_width, self.center_height, self.s)
+        self.helper.paint(painter, event, self.center_width, self.center_height, self.segments_array, self.points_array)
         painter.end()
 
     def mouseMoveEvent(self, event):
+        # Это для отслеживания передвижения мышки, когда рисуем линию и нажата левая
         if self.drag_to_print:
             x = event.x()
             y = event.y()
             self.helper.point_end = [x - self.center_width, y - self.center_height]
-            self.s[-1] = Segment.from_points(self.helper.point_start[0],
-                                             self.helper.point_start[1],
-                                             self.helper.point_end[0],
-                                             self.helper.point_end[1])
+            self.segments_array[-1] = Segment.from_points(self.helper.point_start[0],
+                                                          self.helper.point_start[1],
+                                                          self.helper.point_end[0],
+                                                          self.helper.point_end[1])
 
     def mousePressEvent(self, event):
+        # Рисование линий
         if event.button() == Qt.LeftButton:
             x = event.x()
             y = event.y()
             self.drag_to_print = True
             self.helper.point_start = [x - self.center_width, y - self.center_height]
             self.helper.point_end = [x - self.center_width + 1, y - self.center_height + 1]
-            s1 = Segment.from_points(self.helper.point_start[0],
-                                     self.helper.point_start[1],
-                                     self.helper.point_end[0],
-                                     self.helper.point_end[1])
-            self.s.append(s1)
+            s = Segment.from_points(self.helper.point_start[0],
+                                    self.helper.point_start[1],
+                                    self.helper.point_end[0],
+                                    self.helper.point_end[1])
+            self.segments_array.append(s)
+        # Рисование точек
+        elif event.button() == Qt.RightButton:
+            self.drag_to_print = False
+            x = event.x()
+            y = event.y()
+            p = Point.from_coordinates(x - self.center_width, y - self.center_height)
+            self.points_array.append(p)
+        # Для не_рисования линни, когда перетягиваем мышь
         else:
             self.drag_to_print = False
             print('else')
