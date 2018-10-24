1c1aef4bfff5d4ac2abcc0dbd1e3a6b3914392a3
feldlime <feldlime@yandex.ru>
2018-10-24 19:58:08 +0300

    Написал структуру и пример ограничения

--- geometry/restrictions.py
+++ geometry/restrictions.py
@@ -1,10 +1,35 @@
 """Module with classes of geometry restrictions."""
-import
+import sympy
+from sympy import symbols, Eq
+
+
+def get_full_symbol_name(object_name, symbol_name):
+    return f'{object_name}__{symbol_name}'
+
 
 class Restriction:
-    def get_restriction_equation(self, **kwargs):
-        # may be no objects
+    def get_equations(self, **kwargs) -> list:
         raise NotImplementedError
+
+    @staticmethod
+    def _get_symbols(name, symbols_names):
+        string = ' '.join([get_full_symbol_name(name, sym)
+                           for sym in symbols_names])
+        return symbols(string)
+
+
+class FixedPoint(Restriction):
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def get_equations(self, point_name):
+        x, y = self._get_symbols(point_name, ['x', 'y'])
+        equations = [
+            Eq(x, self._x),
+            Eq(y, self._y)
+        ]
+        return equations
 
 
 class FixedLength(Restriction):
@@ -16,8 +41,8 @@ class FixedLength(Restriction):
         """
         self._length = length
 
-    def get_restriction_equation(self, name_segment):
-
+    def get_equations(self, name_segment):
+        pass
 
 
 # more
