04a765d25a648d93eed09be1125ed1a5a2822e4e
Svit <Svyatoslav.EM@gmail.com>
2018-10-25 08:12:12 +0300

    Прописываю виджеты

--- frontend/main.py
+++ frontend/main.py
@@ -19,20 +19,26 @@ class Helper(object):
         self.point_start = [0, 0]
 
     def paint(self, painter, event, center_width, center_height, segments_array, points_array, segments_array_view,
-              points_array_view):
+              points_array_view, mouse_xy):
         painter.fillRect(event.rect(), QBrush(QColor(255, 255, 255)))
         # Настраиваем кисть
         painter.setPen(QPen(Qt.black, 2, Qt.SolidLine))
         painter.save()
         painter.translate(center_width, center_height)
+
+        # Рисуем координаты рядом с мышкой
+        painter.drawText(QPointF(mouse_xy[0] + 15, mouse_xy[1] + 10), str(mouse_xy[0]) + 'x' + str(mouse_xy[1] * (-1)))
+
         # Рисуем все линии
         for line in segments_array:
             to_draw = line.get_base_representation()
             painter.drawLine(*to_draw)
+
         # Рисуем все точки
         for point in points_array:
             to_draw = point.get_base_representation()
             painter.drawEllipse(*to_draw, 2, 2)
+
         # Рисуем выделенные линии и точки
         painter.setPen(QPen(Qt.blue, 4, Qt.SolidLine))
         for line in segments_array_view:
@@ -41,6 +47,7 @@ class Helper(object):
         for point in points_array_view:
             to_draw = point.get_base_representation()
             painter.drawEllipse(*to_draw, 4, 4)
+
         painter.restore()
 
 
@@ -76,6 +83,8 @@ class GLWidget(QOpenGLWidget):
         self.segments_array_view = []
         self.points_array_view = []
 
+        self.mouse_xy = [0, 0]
+
     def animate(self):
         self.update()
 
@@ -84,7 +93,7 @@ class GLWidget(QOpenGLWidget):
         painter.begin(self)
         painter.setRenderHint(QPainter.Antialiasing)
         self.helper.paint(painter, event, self.center_width, self.center_height, self.segments_array, self.points_array,
-                          self.segments_array_view, self.points_array_view)
+                          self.segments_array_view, self.points_array_view, self.mouse_xy)
         painter.end()
 
     # Отслеживание передвижения мыши
@@ -93,8 +102,8 @@ class GLWidget(QOpenGLWidget):
         near_size = 8
 
         # Переводим координаты от мышки, в координаты пространства
-        x = event.x() - self.center_width
-        y = event.y() - self.center_height
+        self.mouse_xy[0] = x = event.x() - self.center_width
+        self.mouse_xy[1] = y = event.y() - self.center_height
 
         # Обнуляем массивы с выделеными объектам
         self.segments_array_view = []
@@ -115,7 +124,6 @@ class GLWidget(QOpenGLWidget):
                                                                                            segment_pos[1]]
             # Проверяем, находиться ли мышка в квадрате сегмента
             if x <= xx[1] and x >= xx[0] and y <= yy[1] and y >= yy[0]:
-                print('xx,yy')
                 # Длинное уравнение нахождения расстояния от точки до прямой
                 d = ((segment_pos[1] - segment_pos[3]) * x + (segment_pos[2] - segment_pos[0]) * y + (
                         segment_pos[0] * segment_pos[3] - segment_pos[2] * segment_pos[1])) / sqrt(
@@ -126,10 +134,10 @@ class GLWidget(QOpenGLWidget):
         # Это для отслеживания передвижения мышки, когда рисуем линию и нажата левая
         if self.drag_to_print:
             self.helper.point_end = [x, y]
-            s =  Segment.from_points(self.helper.point_start[0],
-                                                          self.helper.point_start[1],
-                                                          self.helper.point_end[0],
-                                                          self.helper.point_end[1])
+            s = Segment.from_points(self.helper.point_start[0],
+                                    self.helper.point_start[1],
+                                    self.helper.point_end[0],
+                                    self.helper.point_end[1])
             self.segments_array[-1] = s
             self.segments_array_view.append(s)
 
@@ -147,6 +155,7 @@ class GLWidget(QOpenGLWidget):
                                     self.helper.point_end[0],
                                     self.helper.point_end[1])
             self.segments_array.append(s)
+
         # Рисование точек
         elif event.button() == Qt.RightButton:
             self.drag_to_print = False
@@ -154,6 +163,7 @@ class GLWidget(QOpenGLWidget):
             y = event.y()
             p = Point.from_coordinates(x - self.center_width, y - self.center_height)
             self.points_array.append(p)
+
         # Для не_рисования линни, когда перетягиваем мышь
         else:
             self.drag_to_print = False
@@ -174,7 +184,21 @@ class MainWindow(QtWidgets.QMainWindow, design.Ui_SuperCAD):
 
         # Дописываем design под себя
         self.listView.hide()
+        self.Point_widget.hide()
+        self.Line_widget.hide()
         self.actionShow_elements_table.triggered['bool'].connect(self.triggered_list_view)
+        self.point.clicked['bool'].connect(self.triggered_point_widget)
+        self.line.clicked['bool'].connect(self.triggered_line_widget)
+        self.combine_points.clicked['bool'].connect(self.triggered_combine_points_widget)
+        self.point_on_middle_line.clicked['bool'].connect(self.triggered_point_on_middle_line_widget)
+        self.point_on_line.clicked['bool'].connect(self.triggered_point_on_line_widget)
+        self.parallelism.clicked['bool'].connect(self.triggered_parallelism_widget)
+        self.normal.clicked['bool'].connect(self.triggered_normal_widget)
+        self.horizontally.clicked['bool'].connect(self.triggered_horizontally_widget)
+        self.fix_size.clicked['bool'].connect(self.triggered_fix_size_widget)
+        self.fix_point.clicked['bool'].connect(self.triggered_fix_point_widget)
+        self.fix_length.clicked['bool'].connect(self.triggered_fix_length_widget)
+        self.fix_angle.clicked['bool'].connect(self.triggered_fix_angle_widget)
 
         helper = Helper()
         openGL = GLWidget(helper, self)
@@ -189,6 +213,117 @@ class MainWindow(QtWidgets.QMainWindow, design.Ui_SuperCAD):
         else:
             self.listView.hide()
 
+    def triggered_point_widget(self, change):
+        self.hide_all_footer_widgets()
+        if change:
+            self.statusbar.showMessage('Point')
+            self.Point_widget.show()
+        else:
+            self.statusbar.showMessage('')
+            self.Point_widget.hide()
+
+    def triggered_line_widget(self, change):
+        self.hide_all_footer_widgets()
+        if change:
+            self.statusbar.showMessage('Line')
+            self.Line_widget.show()
+        else:
+            self.statusbar.showMessage('')
+            self.Line_widget.hide()
+
+    def triggered_combine_points_widget(self, change):
+        self.hide_all_footer_widgets()
+        if change:
+            self.statusbar.showMessage('Choose first point to combine')
+            self.Line_widget.show()
+        else:
+            self.statusbar.showMessage('')
+            self.Line_widget.hide()
+
+    def triggered_point_on_middle_line_widget(self, change):
+        self.hide_all_footer_widgets()
+        if change:
+            self.statusbar.showMessage('Choose line to combine with middle of line')
+            self.Line_widget.show()
+        else:
+            self.statusbar.showMessage('')
+            self.Line_widget.hide()
+
+    def triggered_point_on_line_widget(self, change):
+        self.hide_all_footer_widgets()
+        if change:
+            self.statusbar.showMessage('Choose line to combine with line')
+            self.Line_widget.show()
+        else:
+            self.statusbar.showMessage('')
+            self.Line_widget.hide()
+
+    def triggered_parallelism_widget(self, change):
+        self.hide_all_footer_widgets()
+        if change:
+            self.statusbar.showMessage('Choose changeable line')
+            self.Line_widget.show()
+        else:
+            self.statusbar.showMessage('')
+            self.Line_widget.hide()
+
+    def triggered_normal_widget(self, change):
+        self.hide_all_footer_widgets()
+        if change:
+            self.statusbar.showMessage('Choose changeable line')
+            self.Line_widget.show()
+        else:
+            self.statusbar.showMessage('')
+            self.Line_widget.hide()
+
+    def triggered_horizontally_widget(self, change):
+        self.hide_all_footer_widgets()
+        if change:
+            self.statusbar.showMessage('Choose changeable line')
+            self.Line_widget.show()
+        else:
+            self.statusbar.showMessage('')
+            self.Line_widget.hide()
+
+    def triggered_fix_size_widget(self, change):
+        self.hide_all_footer_widgets()
+        if change:
+            self.statusbar.showMessage('Choose line for fix size')
+            self.Line_widget.show()
+        else:
+            self.statusbar.showMessage('')
+            self.Line_widget.hide()
+
+    def triggered_fix_point_widget(self, change):
+        self.hide_all_footer_widgets()
+        if change:
+            self.statusbar.showMessage('Choose point for fix')
+            self.Line_widget.show()
+        else:
+            self.statusbar.showMessage('')
+            self.Line_widget.hide()
+
+    def triggered_fix_length_widget(self, change):
+        self.hide_all_footer_widgets()
+        if change:
+            self.statusbar.showMessage('Choose point for fix length')
+            self.Line_widget.show()
+        else:
+            self.statusbar.showMessage('')
+            self.Line_widget.hide()
+
+    def triggered_fix_angle_widget(self, change):
+        self.hide_all_footer_widgets()
+        if change:
+            self.statusbar.showMessage('Choose first line for fix angle')
+            self.Line_widget.show()
+        else:
+            self.statusbar.showMessage('')
+            self.Line_widget.hide()
+
+    def hide_all_footer_widgets(self):
+        self.Line_widget.hide()
+        self.Point_widget.hide()
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
