d2fdc87125a075d5e69877f991b138384aaad732
feldlime <feldlime@yandex.ru>
2018-10-26 22:13:06 +0300

    Почти сделал привязки

--- bindings.py
+++ bindings.py
@@ -1,18 +1,76 @@
 """Module with classes of geometry bindings."""
 
 from utils import (
-    IncorrectParamType,
     validate_positive_num,
-    validate_coordinates
+    Coordinates,
+    magnitude
 )
-from figures import Point, Segment
 
-from numpy import inf
+import numpy as np
+
+
+class ReferencedToObject:
+    """Interface for objects that are referenced to other object"""
+
+    def set_object_name(self, object_name: str):
+        """Set host object name."""
+        self._object_name = object_name
+
+    def get_object_name(self) -> str:
+        """Get host object name."""
+        return self._object_name
+
+
+class ReferencedToObjects:
+    """Interface for objects that are referenced to other object"""
+
+    def set_object_names(self, object_names: list):
+        """Set host object name."""
+        self._object_names = object_names
+
+    def get_object_names(self) -> list:
+        """Get host object name."""
+        return self._object_names
 
 
 class Binding:
-    """Class of any binding.
+    """Class of binding."""
 
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def check(self, x, y):
+        """Check if given coordinates places in zone of binding.
+
+        Parameters
+        ----------
+        x, y: int or float
+            Coordinates of cursor.
+
+        Returns
+        ------
+        checking_result: int or bool
+            False if cursor is out of binding zone.
+            Distance between cursor and point of binding.
+        """
+        raise NotImplementedError
+
+    def bind(self, *args):
+        """ Return coordinates to bind cursor.
+
+        Returns
+        ------
+        x, y: float
+            Coordinates to bind
+        """
+        raise NotImplementedError
+
+
+class CentralBinding(Binding):
+    """Class of binding with concrete point to bind.
+
+    Parameters
+    ----------
     coordinates: tuple or function
         The object for which this binding is created.
         If tuple, it must be coordinates of point.
@@ -20,21 +78,18 @@ class Binding:
     """
 
     def __init__(self, coordinates, *args, **kwargs):
-        if isinstance(coordinates, tuple):
-            validate_coordinates(coordinates,
-                                 'If coordinates is tuple, it must contain '
-                                 '2 numbers.')
+        super().__init__()
+        self._coordinates = Coordinates(coordinates)
 
-        elif isinstance(coordinates, function):
-            test_coordinates = coordinates()
-            validate_coordinates(test_coordinates,
-                                 'If coordinates is function, it must returns'
-                                 'tuple that contains 2 numbers.')
+    def bind(self, *args):
+        """ Return coordinates to bind cursor.
 
-        else:
-            raise IncorrectParamType('Coordinates must be tuple or function.')
-
-        self._coordinates = coordinates
+        Returns
+        ------
+        x, y: float
+            Coordinates to bind
+        """
+        return self._coordinates()
 
     def check(self, x, y):
         """Check if given coordinates places in zone of binding.
@@ -44,35 +99,33 @@ class Binding:
         x, y: int or float
             Coordinates of cursor.
 
-        Return
+        Returns
         ------
         checking_result: int or bool
             False if cursor is out of binding zone.
             Distance between cursor and point of binding.
         """
-        raise NotImplemented
-
-    def _coordinates(self):
-        if isinstance(self._coordinates, tuple):
-            return self._coordinates
-        else:
-            return self._coordinates()
+        raise NotImplementedError
 
 
-class CircleBinding(Binding):
-    """Class of simple binding with circle zone.
+class CircleBinding(CentralBinding):
+    """Class of binding with circle zone.
 
+    Parameters
+    ----------
     coordinates: tuple or function
         The object for which this binding is created.
         If tuple, it must be coordinates of point.
         If function, it must be function, that returns coordinates of point.
+    radius: int or float
+        Radius of zone to bind.
     """
 
-    def __init__(self, coordinates, radius, *args, **kwargs):
+    def __init__(self, coordinates, radius):
         super().__init__(coordinates)
 
         validate_positive_num(radius, 'radius')
-        self._radius_sqr = radius ** 2
+        self._radius = radius
 
     def check(self, x, y):
         """Check if given coordinates places in zone of binding.
@@ -82,58 +135,128 @@ class CircleBinding(Binding):
         x, y: int or float
             Coordinates of cursor.
 
-        Return
-        ------
-        checking_result: int or bool
-            False if cursor is out of binding zone.
+        Returns
+        -------
+        checking_result: int or None
+            None if cursor is out of binding zone.
             Distance between cursor and point of binding.
         """
-        base_x, base_y = self._coordinates()
-        distance_sqr = (x - base_x) ** 2 + (y - base_y) ** 2
-        if distance_sqr > self._radius_sqr:
+        base_x, base_y = self._coordinates.get()
+        distance = magnitude(x, y, base_x, base_y)
+        if distance > self._radius:
             return False
         else:
-            return distance_sqr ** 0.5
+            return distance
 
 
-class ObjectCircleBinding(CircleBinding):
-    def __init__(self, coordinates, radius, object_name):
-        super().__init__(coordinates, radius)
-        self.object_name = object_name
-
-
-class PointBinding(ObjectCircleBinding):
+class PointBinding(CircleBinding, ReferencedToObject):
     pass
 
 
-class SegmentStartBinding(ObjectCircleBinding):
+class SegmentStartBinding(CircleBinding, ReferencedToObject):
     pass
 
 
-class SegmentEndBinding(ObjectCircleBinding):
+class SegmentEndBinding(CircleBinding, ReferencedToObject):
     pass
 
 
-class SegmentCenterBinding(ObjectCircleBinding):
+class SegmentCenterBinding(CircleBinding, ReferencedToObject):
     pass
 
 
-class SegmentsIntersectionsBinding(CircleBinding):
-    def __init__(self, coordinates, radius, segment_1_name, segment_2_name):
-        super().__init__(coordinates, radius)
-        self.segment_1_name = segment_1_name
-        self.segment_2_name = segment_2_name
+class SegmentsIntersectionsBinding(CircleBinding, ReferencedToObjects):
+    pass
 
 
-class SegmentBinding(Binding):
+class SegmentBinding(Binding, ReferencedToObject):
     """Binding to all segment (not to point) to highlight it."""
-    pass
+    def __init__(self, coordinates_1, coordinates_2, radius):
+        super().__init__()
+        self._coordinates_1 = Coordinates(coordinates_1)
+        self._coordinates_2 = Coordinates(coordinates_2)
+        validate_positive_num(radius, 'radius')
+        self._radius = radius
+
+    def check(self, x, y):
+        """Check if given coordinates places in zone of binding.
+
+        Parameters
+        ----------
+        x, y: int or float
+            Coordinates of cursor.
+
+        Returns
+        -------
+        checking_result: int or bool
+            None if cursor is out of binding zone.
+            Distance between cursor and point of binding.
+        """
+        x1, y1 = self._coordinates_1.get()
+        x2, y2 = self._coordinates_2.get()
+        distance = self._get_min_distance(x1, y1, x2, y2, x, y)
+
+        if distance > self._radius:
+            return None
+        else:
+            return distance
+
+    def bind(self, x, y):
+        """ Return coordinates to bind cursor.
+
+        Parameters
+        ----------
+        x, y: int or float
+            Coordinates of cursor.
+
+        Returns
+        ------
+        x, y: float
+            Coordinates to bind.
+        """
+        x1, y1 = self._coordinates_1.get()
+        x2, y2 = self._coordinates_2.get()
+        return self._get_nearest_point(x1, y1, x2, y2, x, y)
+
+    @classmethod
+    def _get_min_distance(cls, x1, y1, x2, y2, x, y):
+        """Calculate minimal distance from point to segment."""
+        pass
+
+    @classmethod
+    def _get_nearest_point(cls, x1, y1, x2, y2, x, y):
+        """Calculate point at the segment that are the nearest to the given
+        point.
+        """
+
+        # TODO
+        pass
 
 
-def choose_best_binding(bindings: Binding, x, y):
+def choose_best_binding(bindings: list, x, y):
+    """Choose the nearest binding.
+    Choose only if coordinates are in binding zone.
+
+    Parameters
+    ----------
+    bindings: list of Binding instances
+        List of bindings.
+    x, y: float
+        Coordinates to measure distance (usually, coordinates of cursor).
+
+    Returns
+    -------
+    best_binding: Binding or None
+        Nearest binding to given coordinates.
+        None if no bindings near the coordinates.
+    """
+    if not bindings:
+        return None
+
     def key_fun(binding):
         dist = binding.check(x, y)
-        return dist if dist is not None else inf
+        return dist if dist is not None else np.inf
 
-    bindings = sorted(bindings, key=key_fun, reverse=True)
-    return bindings[0]
+    bindings = sorted(bindings, key=key_fun)
+    best_binding = bindings[0] if not np.isinf(bindings[0]) else None
+    return best_binding
