796d4a90a18c41e88e184907e7b7315729f0e346
feldlime <feldlime@yandex.ru>
2018-11-02 09:23:08 +0300

    Уменьшил число ограничений

--- restrictions.py
+++ restrictions.py
@@ -1,6 +1,6 @@
 """Module with classes of geometry restrictions."""
 
-from utils import ReferencedToObjects
+from utils import ReferencedToObjects, IncorrectParamValue
 from figures import Point, Segment
 
 # noinspection PyUnresolvedReferences
@@ -86,59 +86,40 @@ class SegmentFixed(Restriction, ReferencedToObjects):
         return equations
 
 
-class SegmentStartFixed(Restriction, ReferencedToObjects):
+class SegmentSpotFixed(Restriction, ReferencedToObjects):
     object_types = [Segment]
 
-    @contract(x='number', y='number')
-    def __init__(self, x, y):
+    @contract(x='number', y='number', spot_type='str')
+    def __init__(self, x, y, spot_type):
         super().__init__()
         self._x = x
         self._y = y
 
-    @contract(symbols='dict')
-    def get_equations(self, symbols: dict):
-        x1, y1 = symbols['x1'], symbols['y1']
-        equations = [
-            Eq(x1, self._x),
-            Eq(y1, self._y),
-        ]
-        return equations
-
-
-class SegmentEndFixed(Restriction, ReferencedToObjects):
-    object_types = [Segment]
-
-    @contract(x='number', y='number')
-    def __init__(self, x, y):
-        super().__init__()
-        self._x = x
-        self._y = y
-
-    @contract(symbols='dict')
-    def get_equations(self, symbols: dict):
-        x2, y2 = symbols['x2'], symbols['y2']
-        equations = [
-            Eq(x2, self._x),
-            Eq(y2, self._y),
-        ]
-        return equations
-
-
-class SegmentCenterFixed(Restriction, ReferencedToObjects):
-    object_types = [Segment]
-
-    @contract(x='number', y='number')
-    def __init__(self, x, y):
-        super().__init__()
-        self._x = x
-        self._y = y
+        if spot_type not in ('start', 'end', 'center'):
+            raise IncorrectParamValue(f'Incorrect spot_type {spot_type}.')
+        self._spot_type = spot_type
 
     @contract(symbols='dict')
     def get_equations(self, symbols: dict):
         x1, y1 = symbols['x1'], symbols['y1']
         x2, y2 = symbols['x2'], symbols['y2']
-        # TODO
-        pass
+
+        if self._spot_type == 'start':
+            equations = [
+                Eq(x1, self._x),
+                Eq(y1, self._y),
+            ]
+        elif self._spot_type == 'end':
+            equations = [
+                Eq(x2, self._x),
+                Eq(y2, self._y),
+            ]
+        else:  # center
+            equations = [
+                # TODO
+            ]
+
+        return equations
 
 
 class SegmentLengthFixed(Restriction, ReferencedToObjects):
@@ -216,9 +197,57 @@ class SegmentsPerpendicular(Restriction, ReferencedToObjects):
         pass
 
 
+class SegmentsSpotsJoint(Restriction, ReferencedToObjects):
+    object_types = [Segment, Segment]
+
+    @contract(spot1_type='str', spot2_type='str')
+    def __init__(self, spot1_type, spot2_type):
+        super().__init__()
+
+        if spot1_type not in ('start', 'end', 'center'):
+            raise IncorrectParamValue(f'Incorrect spot1_type {spot1_type}.')
+        self._spot1_type = spot1_type
+
+        if spot2_type not in ('start', 'end', 'center'):
+            raise IncorrectParamValue(f'Incorrect spot2_type {spot2_type}.')
+        self._spot2_type = spot2_type
+
+    @contract(symbols_point='dict', symbols_segment='dict')
+    def get_equations(self, symbols_point: dict, symbols_segment: dict):
+        # TODO
+
+        if self._spot1_type == 'start':
+            equations = [
+
+            ]
+        elif self._spot1_type == 'end':
+            equations = [
+
+            ]
+        else:  # center
+            equations = [
+
+            ]
+
+        if self._spot2_type == 'start':
+            equations = [
+
+            ]
+        elif self._spot2_type == 'end':
+            equations = [
+
+            ]
+        else:  # center
+            equations = [
+
+            ]
+
+        return equations
+
+
 # Point and Segment
 
-class PointFixedOnSegment(Restriction, ReferencedToObjects):
+class PointOnSegmentFixed(Restriction, ReferencedToObjects):
     object_types = [Point, Segment]
 
     @contract(ratio='number,>0,<1')
@@ -240,28 +269,31 @@ class PointOnSegmentLine(Restriction, ReferencedToObjects):
         pass
 
 
-class PointAndSegmentStartJoint(Restriction, ReferencedToObjects):
+class PointAndSegmentSpotJoint(Restriction, ReferencedToObjects):
     object_types = [Point, Segment]
+
+    @contract(spot_type='str')
+    def __init__(self, spot_type):
+        super().__init__()
+        if spot_type not in ('start', 'end', 'center'):
+            raise IncorrectParamValue(f'Incorrect spot_type {spot_type}.')
+        self._spot_type = spot_type
 
     @contract(symbols_point='dict', symbols_segment='dict')
     def get_equations(self, symbols_point: dict, symbols_segment: dict):
         # TODO
-        pass
 
+        if self._spot_type == 'start':
+            equations = [
 
-class PointAndSegmentEndJoint(Restriction, ReferencedToObjects):
-    object_types = [Point, Segment]
+            ]
+        elif self._spot_type == 'end':
+            equations = [
 
-    @contract(symbols_point='dict', symbols_segment='dict')
-    def get_equations(self, symbols_point: dict, symbols_segment: dict):
-        # TODO
-        pass
+            ]
+        else:  # center
+            equations = [
 
+            ]
 
-class PointAndSegmentCenterJoint(Restriction, ReferencedToObjects):
-    object_types = [Point, Segment]
-
-    @contract(symbols_point='dict', symbols_segment='dict')
-    def get_equations(self, symbols_point: dict, symbols_segment: dict):
-        # TODO
-        pass
+        return equations
