14cb07c558355269e5b9102d54e2b313a7d6dece
Svit <Svyatoslav.EM@gmail.com>
2018-11-16 19:58:00 +0300

    Что то запушил

--- tests/test_bindings.py
+++ tests/test_bindings.py
@@ -15,7 +15,13 @@ from utils import (
 
 # noinspection PyTypeChecker
 class TestBindings:
-    def test_normal_creation(self):
-        f = Figure((1, 2), 1)
-        fisg = [f]
-        b = Binding(fisg, circle_bindings_radius=8, segment_bindings_margin=2)
+    def test_create_bindings(self):
+        dict_figures = {}
+        iterator = 0
+        for coo in random.random((10, 2)) * 100 - 50:
+            iterator += 1
+            p = Point((coo[0], coo[1]))
+            dict_figures["fig " + str(iterator)] = p
+        b = create_bindings(dict_figures, circle_bindings_radius=8,
+                            segment_bindings_margin=2)
+
