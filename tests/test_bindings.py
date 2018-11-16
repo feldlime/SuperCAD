eced3a4a2a3aaa33e2af268ddbda673ec6c5869f
Svit <Svyatoslav.EM@gmail.com>
2018-11-16 13:38:01 +0300

    Начал писать test_bindings

--- tests/test_bindings.py
+++ tests/test_bindings.py
@@ -1,7 +1,21 @@
 import pytest
-from numpy import isclose
+from numpy import isclose, pi
+import numpy.random as random
 from bindings import *
+from figures import Figure
 
+from figures import Figure, Point, Segment
+from utils import (
+    IncorrectParamType,
+    IncorrectParamValue,
+    IncorrectParamError
+)
 # TODO:
 
 
+# noinspection PyTypeChecker
+class TestBindings:
+    def test_normal_creation(self):
+        f = Figure((1, 2), 1)
+        fisg = [f]
+        b = Binding(fisg, circle_bindings_radius=8, segment_bindings_margin=2)
